74b64dea2887f8df83d25d805dc9429f6821b90a
Neillycan <neil.chang.project@gmail.com>
2024-05-21 15:35:01 +0800

    Update 543.二叉树的直径.rb

--- 543.二叉树的直径.rb
+++ 543.二叉树的直径.rb
@@ -1,3 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+
 def diameter_of_binary_tree(root)
     @ans = 1
     depth(root)
