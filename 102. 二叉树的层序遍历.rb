6b6124a9970a55a75d3dc0d23ef0930c3f974533
Neillycan <neil.chang.project@gmail.com>
2024-05-21 18:13:08 +0800

    Update 102. 二叉树的层序遍历.rb

--- 102. 二叉树的层序遍历.rb
+++ 102. 二叉树的层序遍历.rb
@@ -9,6 +9,8 @@
 # end
 # @param {TreeNode} root
 # @return {Integer[][]}
+
+#******************递归***********************
 def level_order(root)
     @res = []
     dfs(root,0)
@@ -22,3 +24,25 @@ def dfs(node,level)
     dfs(node.left,level+1)
     dfs(node.right,level+1)
 end
+
+#******************迭代***********************
+
+def level_order(root)
+    return [] if root.nil?
+
+    res = []
+    ans = [root]
+    while ans.length > 0
+        item = []
+        len = ans.length
+        len.times do 
+            node = ans.shift
+            item << node.val
+            ans << node.left if node.left 
+            ans << node.right if node.right
+        end
+        res << item
+    end
+    return res
+end
+
