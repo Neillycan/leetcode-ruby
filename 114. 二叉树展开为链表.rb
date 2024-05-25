4b0e56ba68b19a4c54ff2206e3f9d44e7fff0aa0
Neillycan <neil.chang.project@gmail.com>
2024-05-25 16:54:01 +0800

    Update 114. 二叉树展开为链表.rb

--- 114. 二叉树展开为链表.rb
+++ 114. 二叉树展开为链表.rb
@@ -50,3 +50,29 @@ def flatten(root)
     end
 
 end
+
+# 方法三：寻找前驱节点
+def flatten(root)
+    return if root.nil?
+    curr = root
+    while !curr.nil?
+        # 如果其左子节点不为空
+        if !curr.left.nil?
+            left = curr.left
+            predecessor = left
+            # 则在其左子树中找到最右边的节点，作为前驱节点
+            while !predecessor.right.nil?
+                predecessor = predecessor.right
+            end
+            # 将当前节点的右子节点赋给前驱节点的右子节点
+            predecessor.right = curr.right
+            # 然后将当前节点的左子节点赋给当前节点的右子节点，并将当前节点的左子节点设为空。
+            curr.left = nil
+            curr.right = left
+        end
+        # 对当前节点处理结束后，继续处理链表中的下一个节点，直到所有节点都处理结束。
+        curr = curr.right
+    end
+
+end
+
